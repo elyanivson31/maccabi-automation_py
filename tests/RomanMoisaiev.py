887e650a5a0b1c62b3720e03f0edbcafe8b4eb49
Yaniv Eliav <yanive@lightico.com>
2025-07-28 15:23:20 +0300

    changed tests names

---
+++ tests/RomanMoisaiev.py
@@ -0,0 +1,96 @@
+import uuid
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+from flows.confirm_appointment_flow import ConfirmAppointmentFlow
+from flows.set_appointment_flow import SetAppointmentFlow
+from flows.web_flow import WebFlow
+from flows.main_flow import MainFlow
+from infra.data_loader import DataLoader
+from api.maccabi_api import call_maccabi_search_api
+from datetime import datetime
+from utils.appointment_utils import is_appointment_sooner_than_threshold
+from utils.notifier import notify_telegram_channel
+
+
+def test_open_new_appointment(driver: WebDriver):
+    data_loader = DataLoader()
+    contact = data_loader.get_contact_by_name("yaniv")
+    selected_patient = contact["selectedPatient"]
+
+    threshold_str = data_loader.get_logic_setting("appointmentThresholdDate")
+    threshold_date = datetime.fromisoformat(threshold_str)
+
+    service_name = data_loader.get_logic_setting("appointmentServiceName")
+    city_name = data_loader.get_logic_setting("appointmentDoctorCity")
+    appointment_type = data_loader.get_logic_setting("appointmentType")
+
+    # Create and reuse WebFlow instance
+    web_flow = WebFlow(driver)
+
+    web_flow.login_flow().login_to_portal(contact)
+    
+    web_flow.main_flow().switch_to_patient(selected_patient)
+
+    web_flow.main_flow().start_new_appointment()
+
+    if "doctorName" not in contact:
+        raise ValueError("Missing 'doctorName' in contact settings.")
+    
+    # Assume you're logged in
+    payload = {
+            "DocName": contact["doctorName"],
+            "ChapterId": "001",
+            "InitiatorCode": "001",
+            "isKosher": 0,
+            "IsMobileApplication": 0,
+            "PageNumber": 1,
+            "RequestId": str(uuid.uuid4())
+
+    }
+
+    response = call_maccabi_search_api(driver, payload)
+    response_json = response.json()
+    assert response.status_code == 200
+    print(response.json())
+
+
+    # assert is_appointment_sooner_than_threshold(response_json, threshold_date)
+
+
+    if is_appointment_sooner_than_threshold(response_json, threshold_date):
+        notify_telegram_channel(
+    f"🎉 נמצא תור מוקדם!\n"
+    f"👤 מטופל: {contact["selectedPatient"]}\n"
+    f"🧑‍⚕️ רופא: {contact["doctorName"]}\n"
+    f"🗓️ תאריך זמין: {threshold_date}\n"
+)
+
+
+    web_flow.set_appointment_flow().continue_to_doctor_search()
+    web_flow.set_appointment_flow().choose_service(service_name)
+    web_flow.find_doctor_flow().search_for_doctor(contact["doctorName"], city_name)
+    
+
+    web_flow.find_doctor_flow().select_set_appointment()
+    
+    web_flow.choose_appointment_type_flow().select_appointment_type_and_continue(
+    appointment_type)
+
+    web_flow.choose_appointment_type_flow().select_under_18_if_present()
+    web_flow.choose_appointment_type_flow().agree_to_come_with_personal_health_card()
+
+
+    actual_date = web_flow.confirm_appointment_flow().get_displayed_appointment_date()
+
+    assert actual_date.date() <= threshold_date.date(), (
+    f"❌ Appointment date mismatch!\n"
+    f"🟡 Expected (threshold): {threshold_date.strftime('%d/%m/%Y')}\n"
+    f"🔵 Displayed on page:   {actual_date.strftime('%d/%m/%Y')}"
+)
+    
+    web_flow.confirm_appointment_flow().choose_time_slot("latest");
+
+    web_flow.confirm_appointment_flow().confirm_appointment()
+
+    web_flow.confirm_appointment_flow().confirm_and_continue()
+    web_flow.confirm_appointment_flow().verify_appointment_success()
