176d66e240593c24217b9214c494b34914eda225
Yaniv Eliav <yanive@lightico.com>
2025-08-05 19:12:19 +0300

    chrome changes

--- infra/driver_factory.py
+++ infra/driver_factory.py
@@ -14,7 +14,13 @@ def create_chrome_driver():
     # options.add_argument("--headless")
     # Start Chrome maximized to ensure elements are visible
     options.add_argument("--start-maximized")
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')  # safe on some systems
+    options.add_argument('--remote-debugging-port=9222')  # avoids Runtime.evaluate error
 
+    # your test logic here
 
     # Additional Chrome options (if needed) can be added, for example:
     # options.add_argument("--disable-gpu")
