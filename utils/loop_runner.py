57e0014b861289c52f74c03a5c75ab48e646b0c1
Yaniv Eliav <yanive@lightico.com>
2025-08-05 18:27:17 +0300

    remove loop

--- utils/loop_runner.py
+++ utils/loop_runner.py
@@ -1,35 +1,71 @@
-# utils/loop_runner.py
-import time
-from datetime import datetime, timedelta
 from selenium import webdriver
 import sys
 import os
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from tests.elon_shlomo import elon_shlomo
 
-
-
-def run_loop_for_max_duration(max_minutes=600, interval_minutes=30):
-    end_time = datetime.now() + timedelta(minutes=max_minutes)
-
-    while datetime.now() < end_time:
-        print("\n🔄 Running test...")
-        driver = webdriver.Chrome()
-        try:
-            success = elon_shlomo(driver)
-            if success:
-                print("✅ Test passed. Stopping loop.")
-                break
-            else:
-                print("❌ No appointment found. Retrying in 10 minutes...")
-        except Exception as e:
-            print(f"⚠️ Test raised an exception: {e}")
-            print("⏱️ Will retry in 10 minutes...")
-        finally:
-            driver.quit()
-
-        time.sleep(interval_minutes * 60)
-
+def main():
+    print("🔄 Running test once...")
+    driver = webdriver.Chrome()
+    try:
+        success = elon_shlomo(driver)
+        if success:
+            print("✅ Test passed.")
+        else:
+            print("❌ No appointment found.")
+    except Exception as e:
+        print(f"⚠️ Test raised an exception: {e}")
+    finally:
+        driver.quit()
 
 if __name__ == "__main__":
-    run_loop_for_max_duration()
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # utils/loop_runner.py
+# import time
+# from datetime import datetime, timedelta
+# from selenium import webdriver
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# from tests.elon_shlomo import elon_shlomo
+
+
+
+# def run_loop_for_max_duration(max_minutes=900, interval_minutes=20):
+#     end_time = datetime.now() + timedelta(minutes=max_minutes)
+
+#     while datetime.now() < end_time:
+#         print("\n🔄 Running test...")
+#         driver = webdriver.Chrome()
+#         try:
+#             success = elon_shlomo(driver)
+#             if success:
+#                 print("✅ Test passed. Stopping loop.")
+#                 break
+#             else:
+#                 print("❌ No appointment found. Retrying in 10 minutes...")
+#         except Exception as e:
+#             print(f"⚠️ Test raised an exception: {e}")
+#             print("⏱️ Will retry in 10 minutes...")
+#         finally:
+#             driver.quit()
+
+#         time.sleep(interval_minutes * 60)
+
+
+# if __name__ == "__main__":
+#     run_loop_for_max_duration()
